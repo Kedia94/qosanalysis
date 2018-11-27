f791fe3d3f9f7ce8e87e0c8de624b66d2b0da498
Kedia94 <kedia94@gmail.com>
2018-11-27 18:50:58 +0900

    Change task list

--- config.h
+++ config.h
@@ -5,13 +5,13 @@
 
 #define TASK0 "qqou0.txt"
 #define TASK1 "qqou1.txt"
-#define TASK2 "qqou2.txt"
-#define TASK3 "qqou3.txt"
+#define TASK2 "qqqou2.txt"
+#define TASK3 "qqqou3.txt"
 #define TASK4 "qqou4.txt"
-#define TASK5 "qqou5.txt"
-#define TASK6 "qqou6.txt"
-#define TASK7 "qqou7.txt"
-#define TASK8 "qqou8.txt"
+#define TASK5 "qqqou5.txt"
+#define TASK6 "qqqou6.txt"
+#define TASK7 "qqqou7.txt"
+#define TASK8 "qqqou8.txt"
 #define TASK9 "qqou9.txt"
 
 #define CON(x) x+std::string(".con")
